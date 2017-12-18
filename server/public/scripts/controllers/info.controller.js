c8f760db63e73329aa55d1fc9301d254e8d8bc6f
Laura Goetz <goetzlaura612@gmail.com>
2017-12-17 22:38:18 -0600

    added search functionality

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -4,33 +4,45 @@ myApp.controller('InfoController', function($http) {
   var steam_id = '';
   var api_key = '502CF7AD2FE8F1911591043E913B519D';
   var testResult = [];
-  var game_count;
   var user_result = [];
 
- 
+
+ /*
   vm.firstTest = function () {
 
       $http.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=502CF7AD2FE8F1911591043E913B519D&steamid=76561198096885956&include_appinfo=1&format=json').then(function (response) {
-          console.log('this is the response', response.data.response.games);
+
           vm.testResult = response.data.response.games;
-          console.log('testResult', vm.testResult);
+
       });
   }
 
   vm.firstTest();
 
- 
+ */
 
 
 
   vm.userTest = function () {
 
       $http.get(' http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=502CF7AD2FE8F1911591043E913B519D&steamids=76561198096885956').then(function (response) {
-          console.log('this is the response', response.data.response.players);
+
           vm.user_result = response.data.response.players;
-          console.log('testResult', vm.user_result);
+
       });
   }
 
   vm.userTest();
+
+
+  vm.findGames = function (steamid) {
+      console.log('steamid', steamid);
+     
+
+      $http.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + api_key + '&steamid=' + steamid + '&include_appinfo=1&format=json').then(function (response) {
+
+          vm.testResult = response.data.response.games;
+
+      });
+  }
 });
